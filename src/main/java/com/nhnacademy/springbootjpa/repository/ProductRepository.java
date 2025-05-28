319092ff6e423bda1d2ce429c3045e3eed211240
pear_c <zxcv9676@naver.com>
2025-05-28 20:07:42 +0900

    feat: Repository 보강 - @Query 어노테이션 사용

--- src/main/java/com/nhnacademy/springbootjpa/repository/ProductRepository.java
+++ src/main/java/com/nhnacademy/springbootjpa/repository/ProductRepository.java
@@ -2,10 +2,19 @@ package com.nhnacademy.springbootjpa.repository;
 
 import com.nhnacademy.springbootjpa.entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 
 public interface ProductRepository extends JpaRepository<Products, Long> {
     List<Products> findByCategories_CategoryId(long categoryId);
+
+    @Query("SELECT p FROM Products p WHERE p.uniCost > :uniCost")
+    List<Products> findAllByUniCostAtLeastByQueryParam(long uniCost);
+
+    @Modifying
+    @Query("UPDATE Products p SET p.modelName = :modelName WHERE p.productId = :productId")
+    void updateModelNameByProductId(String modelName, long productId);
 }
